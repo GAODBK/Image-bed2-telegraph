6e36ec8a146411fe0186fc68dac217013fc338e9
GAODBK <154538773+GAODBK@users.noreply.github.com>
2024-10-18 17:32:15 +0800

    Update Tooltip.jsx

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,44 +1,44 @@
 import React from "react";
 
 const TooltipItem = ({ children, tooltipsText, position }) => {
+  const baseTooltipClass = `absolute bg-slate-200 z-20 whitespace-nowrap rounded bg-primary px-4 py-[6px] text-sm font-semibold opacity-0 group-hover:opacity-100`;
+  const baseArrowClass = `absolute -z-10 h-2 w-2 rotate-45 rounded-sm bg-primary`;
+
+  let positionTooltipClass = '';
+  let positionArrowClass = '';
+
+  switch (position) {
+    case "right":
+      positionTooltipClass = 'left-full top-1/2 ml-3 -translate-y-1/2';
+      positionArrowClass = 'left-[-3px] top-1/2 -translate-y-1/2';
+      break;
+    case "top":
+      positionTooltipClass = 'bottom-full left-1/2 mb-3 -translate-x-1/2';
+      positionArrowClass = 'bottom-[-3px] left-1/2 -translate-x-1/2';
+      break;
+    case "left":
+      positionTooltipClass = 'right-full top-1/2 mr-3 -translate-y-1/2';
+      positionArrowClass = 'right-[-3px] top-1/2 -translate-y-1/2';
+      break;
+    case "bottom":
+      positionTooltipClass = 'left-1/2 top-full mt-3 -translate-x-1/2';
+      positionArrowClass = 'left-1/2 top-[-3px] -translate-x-1/2';
+      break;
+    default:
+      break; // 或者根据需要设置一个默认值
+  }
+
   return (
-    <div className="">
-      <div className="">
-        <div className="group relative inline-block">
-          <button className="inline-flex rounded">
-            {children}
-          </button>
-          <div
-            className={` ${
-              (position === "right" &&
-                `absolute bg-slate-200 left-full top-1/2 z-20 ml-3 -translate-y-1/2 whitespace-nowrap rounded bg-primary px-4 py-[6px] text-sm font-semibold  opacity-0 group-hover:opacity-100`) ||
-              (position === "top" &&
-                `absolute bg-slate-200 bottom-full left-1/2 z-20 mb-3 -translate-x-1/2 whitespace-nowrap rounded bg-primary px-4 py-[6px] text-sm font-semibold  opacity-0 group-hover:opacity-100`) ||
-              (position === "left" &&
-                `absolute bg-slate-200 right-full top-1/2 z-20 mr-3 -translate-y-1/2 whitespace-nowrap rounded bg-primary px-4 py-[6px] text-sm font-semibold opacity-0 group-hover:opacity-100`) ||
-              (position === "bottom" &&
-                `absolute bg-slate-200 left-1/2 top-full z-20 mt-3 -translate-x-1/2 whitespace-nowrap rounded bg-primary px-4 py-[6px] text-sm font-semibold  opacity-0 group-hover:opacity-100`)
-            }`}
-          >
-            <span
-              className={` ${
-                (position === "right" &&
-                  `absolute  left-[-3px] top-1/2 -z-10 h-2 w-2 -translate-y-1/2 rotate-45 rounded-sm bg-primary`) ||
-                (position === "top" &&
-                  `absolute  bottom-[-3px] left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rotate-45 rounded-sm bg-primary`) ||
-                (position === "left" &&
-                  `absolute  right-[-3px] top-1/2 -z-10 h-2 w-2 -translate-y-1/2 rotate-45 rounded-sm bg-primary`) ||
-                (position === "bottom" &&
-                  `absolute  left-1/2 top-[-3px] -z-10 h-2 w-2 -translate-x-1/2 rotate-45 rounded-sm bg-primary`)
-              } `}
-            ></span>
-            {tooltipsText}
-          </div>
-        </div>
+    <div className="relative inline-block">
+      <button className="inline-flex rounded">
+        {children}
+      </button>
+      <div className={`${baseTooltipClass} ${positionTooltipClass}`}>
+        <span className={`${baseArrowClass} ${positionArrowClass}`}></span>
+        {tooltipsText}
       </div>
     </div>
   );
 };
-
 
 export default TooltipItem;
